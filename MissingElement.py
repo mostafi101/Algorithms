be8e275fd77bfd4f35323894a2988faf2c2e292d
mostafij101 <p.mostafij@gmail.com>
2016-12-20 22:01:47 -0600

    Missing Elements

--- MissingElement.py
+++ MissingElement.py
@@ -5,3 +5,21 @@ Created on Tue Dec 20 21:21:37 2016
 @author: mostafijurrahaman
 """
 
+def finder(arr1, arr2):
+    count = {}
+    output = list()
+    
+    for num in arr2:
+        if num in count:
+            count[num] += 1
+        else:
+            count[num] = 1
+    
+    for num in arr1:
+        if num not in count or count[num] == 0:
+            output.append(num)
+        else:
+            count[num] -= 1
+    return output
+    
+finder([1,2,3,4,5,6],[6,4,3])
